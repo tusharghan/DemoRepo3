596e8390d67e11257558163d84b48caab9ce6277
tusharghan <tushar.ghan@gmail.com>
2018-03-24 14:33:33 +0530

    first commit

---
+++ src/main/java/qa/selenium/Git/qa/selenium/Git/App.java
@@ -0,0 +1,13 @@
+package qa.selenium.Git.qa.selenium.Git;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
